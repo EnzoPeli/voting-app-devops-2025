cb83ec8567906ecd55d4b6ffaa80cec1b42b8cf9
Enzo Pelizzari <enzozopeli@gmail.com>
2025-06-26 23:07:23 -0300

    fix ami version

--- infra/modules/eks_cluster/main.tf
+++ infra/modules/eks_cluster/main.tf
@@ -22,7 +22,7 @@ resource "aws_eks_node_group" "this" {
   }
 
   instance_types = var.instance_types
-  ami_type       = "AL2023_x86_64"
+  ami_type       = "AL2023_x86_64_STANDARD"
 
   remote_access {
     ec2_ssh_key = var.ec2_ssh_key_name
